b113a478b50c79cedf31f71383c7b82c77ea84cc
Julio Faerman <jfaerman@gmail.com>
2012-11-29 18:16:27 -0200

    Separated virtual hosts for API and GWT app, with proper CORS configuration.

---
+++ kornell-api/src/main/scala/com/craftware/kornell/api/RootResource.scala
@@ -0,0 +1,12 @@
+package com.craftware.kornell.api
+
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.GET
+
+@Path("")
+class RootResource {
+	@Produces(Array("text/plain"))
+	@GET
+	def get = "Welcome to Kornell API"
+}
