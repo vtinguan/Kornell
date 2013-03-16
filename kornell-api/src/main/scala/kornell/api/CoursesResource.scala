70f578898254b9b926b286331ec3d08f3661eb1f
Julio Faerman <jfaerman@gmail.com>
2013-03-15 21:25:42 -0300

    Cleanning a bit

--- kornell-api/src/main/scala/kornell/api/CoursesResource.scala
+++ kornell-api/src/main/scala/kornell/api/CoursesResource.scala
@@ -1,23 +1,13 @@
 package kornell.api
 
 import scala.collection.JavaConverters._
-import javax.inject.Inject
-import javax.persistence.EntityManager
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-import javax.ws.rs.core.Context
-import javax.ws.rs.core.SecurityContext
-import kornell.core.shared.data.BeanFactory
-import kornell.dev.Mocks
+import javax.ws.rs._
+import javax.ws.rs.core._
+import kornell.core.shared.data._
 import kornell.repository.slick.plain.Courses
-import kornell.core.shared.data.CoursesTO
-import javax.ws.rs.PathParam
-import kornell.core.shared.data.CourseTO
 
 @Path("courses")
 class CoursesResource {
-
   @GET
   @Produces(Array(CoursesTO.MIME_TYPE))
   def getCourses(implicit @Context sc: SecurityContext) = 
@@ -28,6 +18,4 @@ class CoursesResource {
   @GET
   def getCourse(@PathParam("uuid") uuid:String)(implicit @Context sc: SecurityContext) = 
     Courses.byUUID(uuid)
- 
- 
 }
