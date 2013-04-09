ce0c372cc5e06ef43aac384d6648f52c3acfda9d
Julio Faerman <jfaerman@gmail.com>
2013-04-08 21:23:11 -0300

    Just trying to close KNL-6

--- kornell-api/src/main/sql/2013/03/001 - Person and Auth.sql
+++ kornell-api/src/main/sql/2013/03/001 - Person and Auth.sql
@@ -3,23 +3,16 @@ CREATE TABLE Person (
   fullName varchar(255)
 );
 
-CREATE TABLE Principal (
-  uuid char(36) NOT NULL PRIMARY KEY,
-  username varchar(255),
-  person_uuid char(36),
-  UNIQUE KEY username (username),
+CREATE TABLE Password (
+  username varchar(63) NOT NULL PRIMARY KEY,
+  password varchar(255) DEFAULT NULL,
+  person_uuid char(36) DEFAULT NULL,
   FOREIGN KEY (person_uuid) REFERENCES Person(uuid)
 );
 
-CREATE TABLE PasswordCredential (
-  uuid char(36) NOT NULL PRIMARY KEY,
-  password varchar(255) DEFAULT NULL,
-  principal_uuid char(36) DEFAULT NULL,
-  FOREIGN KEY (principal_uuid) REFERENCES Principal (uuid)
-);
-
-CREATE TABLE Principal_roles (
-  principal_uuid  char(36) NOT NULL PRIMARY KEY,
-  roles varchar(255) DEFAULT NULL,
-  FOREIGN KEY (principal_uuid) REFERENCES Principal(uuid)
+CREATE TABLE Role (
+  username varchar(63) not null,
+  role varchar(255) not null,
+  foreign key (username) references Password(username),
+  primary key (username, role)
 );
