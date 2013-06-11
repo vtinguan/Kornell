1a5fc39f47d53e8e73535d28f74e1369ef61118e
Julio Faerman <jfaerman@gmail.com>
2013-06-11 14:51:25 -0700

    Should be ready for CI

--- kornell-api/src/main/scala/kornell/api/RootResource.scala
+++ kornell-api/src/main/scala/kornell/api/RootResource.scala
@@ -3,10 +3,18 @@ package kornell.api
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.GET
+import java.util.Properties
 
 @Path("")
 class RootResource {
 	@Produces(Array("text/plain"))
 	@GET
-	def get = "Welcome to Kornell API\n" 
+	def get = {
+	  val version = new Properties();
+	  version.load(getClass().getClassLoader().getResourceAsStream("version.properties")); 
+	  s"""|Welcome to Kornell API\n
+	  |Built on ${version.getProperty("built.on","development environment")}
+	  |
+	  """.stripMargin 
+	}
 }
