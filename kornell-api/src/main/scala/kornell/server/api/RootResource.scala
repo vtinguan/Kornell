be24391b1b6226cfbb08edf761d9ffa3992d40aa
Julio Faerman <jfaerman@gmail.com>
2013-08-25 19:53:52 -0300

    Pinging DB

--- kornell-api/src/main/scala/kornell/server/api/RootResource.scala
+++ kornell-api/src/main/scala/kornell/server/api/RootResource.scala
@@ -4,6 +4,7 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.GET
 import java.util.Properties
+import kornell.server.repository.jdbc.SQLInterpolation._
 
 @Path("")
 class RootResource {
@@ -16,10 +17,13 @@ class RootResource {
 	  if(properties.isDefined){
 		  version.load(properties.get);
 	  }
+	  val two = sql"select 1+1".map {rs => rs.getInt(1)}.head
+	  
 	  s"""|Welcome to Kornell API\n
 	  |
 	  |Built on ${version.getProperty("built.on","development environment")}
-	  |
-	  """.stripMargin 
+	  |MySQL thinks that 1+1=$two
+	  """.stripMargin
+	  
 	}
 }
