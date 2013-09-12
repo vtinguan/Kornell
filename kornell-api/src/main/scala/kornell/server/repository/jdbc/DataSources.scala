eb5dfa89bece3da846adfee93684ab99305d479f
Julio Faerman <jfaerman@gmail.com>
2013-09-11 21:00:18 -0400

    Fixed connection validation to happen only once

--- kornell-api/src/main/scala/kornell/server/repository/jdbc/DataSources.scala
+++ kornell-api/src/main/scala/kornell/server/repository/jdbc/DataSources.scala
@@ -5,8 +5,11 @@ import javax.naming.NoInitialContextException
 import javax.naming.InitialContext
 import javax.naming.Context
 import javax.sql.DataSource
+import java.util.logging._
 
 object DataSources {
+  val log = Logger.getLogger(getClass.getName)
+    
   lazy val KornellDS = {
     val context = new InitialContext()
       .lookup("java:comp/env")
@@ -26,4 +29,16 @@ object DataSources {
   lazy val LOCAL: ConnectionFactory = () =>
     DriverManager.getConnection("jdbc:mysql:///ebdb", "kornell", "42kornell73")
 
+  lazy val connect: ConnectionFactory = verified(JNDI).getOrElse(LOCAL)
+
+  def verified(cf: ConnectionFactory): Option[ConnectionFactory] =
+    try {      
+      val conn = cf()
+      val stmt = conn.createStatement
+      stmt.execute("select 40+2")
+      stmt.close
+      conn.close
+      log.info("Connection Factory validated. Nice!");
+      Some(cf)
+    } catch { case e: Exception => None }
 }
