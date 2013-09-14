50a85c065d0c94ee02088a935facaa291752f943
Tiago Faria <tcfaria@gmail.com>
2013-09-13 23:49:44 -0400

    minor css change

--- kornell-api/src/main/resources/db/migration/2013/09/V2013.09.09.18.50__Person.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS Person (
-  uuid char(36) NOT NULL,
-  fullName varchar(255) DEFAULT NULL,
-  lastPlaceVisited varchar(2083) DEFAULT NULL,
-  PRIMARY KEY (uuid)
-);
--- kornell-api/src/main/resources/db/migration/2013/09/V2013.09.09.18.51__Password.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Password` (
-  username varchar(63) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  person_uuid char(36) DEFAULT NULL,
-  PRIMARY KEY (username),
-  KEY person_uuid (person_uuid),
-  CONSTRAINT Password_ibfk_1 FOREIGN KEY (person_uuid) REFERENCES Person (uuid)
-);
