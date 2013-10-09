0e503fe7b697ee6bf52f41fe9d1a123ad8c01cda
Julio Faerman <jfaerman@gmail.com>
2013-10-08 23:54:38 -0700

    sandboxing...

--- kornell-gwt/src/main/java/kornell/gui/client/presentation/sandbox/generic/GenericSandboxView.java
+++ kornell-gwt/src/main/java/kornell/gui/client/presentation/sandbox/generic/GenericSandboxView.java
@@ -4,17 +4,34 @@ import kornell.gui.client.presentation.sandbox.SandboxView;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class GenericSandboxView  extends Composite implements SandboxView {
 	interface MyUiBinder extends UiBinder<Widget, GenericSandboxView> {}
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-		
+	
+	VerticalPanel vpanel = new VerticalPanel();
+	
 	public GenericSandboxView() {
 		GWT.log("GenericSandboxView()");		
+		panel.add(vpanel);
+		vpanel.add(new Label("Loading..."));
 	    initWidget(uiBinder.createAndBindUi(this));
+	    fetchAndDraw();
 	}
+	
+	private void fetchAndDraw() {
+		panel.clear();
+		vpanel.add(new Label("done"));
+		
+	}
+
+	@UiField FlowPanel panel;
 	
 
 	@Override
--- kornell-gwt/src/main/java/kornell/gui/client/sequence/CourseSequencer.java
+++ kornell-gwt/src/main/java/kornell/gui/client/sequence/CourseSequencer.java
@@ -143,7 +143,6 @@ public class CourseSequencer implements Sequencer {
 				setContents(contents);
 				orientateAndGo();
 			}
-
 		});
 	}
 
