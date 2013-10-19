2f9b0080dc8dc6913aaabc80d59bf6cdfa37bbed
Tiago Faria <tcfaria@gmail.com>
2013-10-19 13:44:43 -0400

    Fix certificate generation

--- kornell-api/src/main/scala/kornell/server/dev/util/ProduceUser.scala
+++ kornell-api/src/main/scala/kornell/server/dev/util/ProduceUser.scala
@@ -6,6 +6,7 @@ import scala.util.Random
 import scala.collection.Seq
 import kornell.server.repository.jdbc.Institutions
 import kornell.core.shared.data.Registration
+import kornell.server.repository.jdbc.PersonRepository
 
 
 object ProduceUser extends App {
@@ -21,10 +22,10 @@ object ProduceUser extends App {
   val institution_uuid = "00a4966d-5442-4a44-9490-ef36f133a259";
   val course_uuid = "d9aaa03a-f225-48b9-8cc9-15495606ac46";
     
-  People().createPerson(fullName)
-		  .setPassword(username, password) 
+  val p: PersonRepository = People().createPerson(fullName)
+  p.setPassword(username, password) 
 		  .registerOn(institution_uuid)
-		  .enrollOn(course_uuid)
+		  .enrollOn(course_uuid, p.get().get.getUUID())
  
   println(" Full Name: "+fullName)
   println("  Username: "+username)
