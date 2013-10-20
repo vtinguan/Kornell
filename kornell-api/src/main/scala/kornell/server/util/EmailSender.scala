f4f0a39049e5a9645b838d1b0a0f9a13bef21515
Tiago Faria <tcfaria@gmail.com>
2013-10-19 23:15:58 -0400

    Change email's subject charset encoding.

--- kornell-api/src/main/scala/kornell/server/util/EmailSender.scala
+++ kornell-api/src/main/scala/kornell/server/util/EmailSender.scala
@@ -56,7 +56,7 @@ object EmailSender {
 			message.setRecipients(Message.RecipientType.TO, person.getEmail())
 			val sex: String = if ("F".equals(person.getSex())) "a" else "o"
 			  
-			message.setSubject("Bem-vind" + sex + " à Universidade Virtual " + institution.getName() + "!" )
+			message.setSubject("Bem-vind" + sex + " à Universidade Virtual " + institution.getName() + "!", "UTF-8");
 			message.setSentDate(new Date())
 			
 			val body: String = """
