8ab993d1c8664d287d4c97bcb3ce1e0fd4d3b141
Julio Faerman <jfaerman@gmail.com>
2013-10-30 21:50:15 -0600

    deploy to s3

--- kornell-gwt/deploy.sh
+++ kornell-gwt/deploy.sh
@@ -7,4 +7,4 @@ secret_key = $AWS_SECRET_KEY
 " > s3cfg
 
 echo Syncing to S3
-./tools/s3cmd -c ./s3cfg  --delete-removed --exclude="WEB-INF/*" --recursive sync target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com.br
+./tools/s3cmd/s3cmd -c ./s3cfg  --delete-removed --exclude="WEB-INF/*" --recursive sync target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com.br
