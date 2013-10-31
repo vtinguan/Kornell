5e098391cba199526d87a31e91adf72ded0df093
Julio Faerman <jfaerman@gmail.com>
2013-10-30 22:36:00 -0600

    Deploy to S3

--- kornell-gwt/deploy.sh
+++ kornell-gwt/deploy.sh
@@ -9,6 +9,5 @@ secret_key = $AWS_SECRET_KEY
 echo Setting API Endpoint...
 echo 'KornellConfig.apiEndpoint = "http://kornell-api-red.elasticbeanstalk.com";' > ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/KornellConfig.js
 echo Deploying to S3...
-./tools/s3cmd/s3cmd -c ./s3cfg  --delete-removed --exclude="WEB-INF/*" --recursive sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com.br
+./tools/s3cmd/s3cmd -c ./s3cfg  --delete-removed --exclude="WEB-INF/*" --recursive sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com
 echo Deployed, you should be good to go.
-
