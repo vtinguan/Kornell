6b8762b0756d4c02064db0b8e8c1f3ca3bc6b881
Julio Faerman <jfaerman@gmail.com>
2013-11-01 12:19:55 -0600

    Deploy to S3

--- kornell-gwt/deploy.sh
+++ kornell-gwt/deploy.sh
@@ -6,8 +6,21 @@ access_key = $AWS_ACCESS_KEY_ID
 secret_key = $AWS_SECRET_KEY
 " > s3cfg
 
-echo Setting API Endpoint...
+echo Setting API Endpoint
 echo 'KornellConfig.apiEndpoint = "http://kornell-api-red.elasticbeanstalk.com";' > ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/KornellConfig.js
-echo Deploying to S3...
-./tools/s3cmd/s3cmd -c ./s3cfg  --delete-removed --exclude="WEB-INF/*" --recursive sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com
-echo Deployed, you should be good to go.
+echo Deploying to S3
+# Cacheable for 30 days (2592000 seconds)
+./tools/s3cmd/s3cmd -c ./s3cfg  \
+  --delete-removed \
+  --exclude='WEB-INF/*'
+  --exclude='*.nocache.js' \
+  --recursive \
+  --add-header "Cache-control: max-age=2592000" \
+  sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com
+
+# Don't cache Kornell.nocache.js
+./tools/s3cmd/s3cmd -c ./s3cfg  \
+  --add-header "Cache-control: max-age=0" \
+  put ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/Kornell/Kornell.nocache.js  s3://eduvem.com
+
+echo Unless you see errors above, you should be deployed and good to go. 
