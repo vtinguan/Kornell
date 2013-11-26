e979618455ffb9270527c5227023053e3edc5c86
Julio Faerman <jfaerman@gmail.com>
2013-11-25 22:15:50 -0600

    Seems fixed

--- kornell-api/src/main/resources/db/migration/2013/11/V2013.11.21.16.28__CourseClass.sql
+++ kornell-api/src/main/resources/db/migration/2013/11/V2013.11.21.16.28__CourseClass.sql
@@ -5,13 +5,12 @@ create table if not exists CourseClass(
 	institution_uuid char(36) not null references Institution(uuid)
 );
 
--- insert into CourseClass(uuid,name,courseVersion_uuid,institution_uuid)
--- select UUID(),"cc0",CV.uuid,C.institution_uuid as courseVersion_uuid 
---  from CourseVersion CV join Course C on CV.course_uuid = C.uuid;
-  
 alter table Course drop column institution_uuid;
 alter table Enrollment drop foreign key Enrollment_Course;
 alter table Enrollment drop column course_uuid;
 alter table Enrollment add column class_uuid char(36) not null references CourseClass(uuid);
+
 alter table ActomEntered drop column course_uuid;
 alter table ActomEntered add column courseClass_uuid char(36) not null references CourseClass(uuid);
+
+--DATA NEEDS FIXING MANUALLY. SORRY :(
--- kornell-api/src/main/scala/kornell/server/repository/jdbc/MigrationListener.scala
+++ kornell-api/src/main/scala/kornell/server/repository/jdbc/MigrationListener.scala
@@ -4,14 +4,24 @@ import javax.servlet.annotation.WebListener
 import javax.servlet.ServletContextListener
 import javax.servlet.ServletContextEvent
 import com.googlecode.flyway.core.Flyway
+import scala.util.Try
+import com.googlecode.flyway.core.api.FlywayException
 
 @WebListener
 class MigrationListener extends ServletContextListener {
   override def contextInitialized(e: ServletContextEvent): Unit = {
-    val flyway = new Flyway();
-    flyway.setDataSource(DataSources.KornellDS)
-    flyway.setLocations("db/");
-    flyway.migrate();
+    try {
+      val flyway = new Flyway();
+      flyway.setDataSource(DataSources.KornellDS)
+      flyway.setLocations("db/");
+      flyway.migrate();
+    } catch {
+      case e:FlywayException => {
+        //TODO: Fail deployment and notify the world
+        e.printStackTrace(System.err)
+        //System.exit(-1)
+      }
+    }
   }
 
   override def contextDestroyed(e: ServletContextEvent): Unit = {
