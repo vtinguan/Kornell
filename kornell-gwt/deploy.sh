1390c40d5a25cca655928d7a91209c1db0e1e2b1
Julio Faerman <jfaerman@gmail.com>
2013-12-27 02:36:07 -0200

    i must have been scripting

--- kornell-gwt/deploy.sh
+++ kornell-gwt/deploy.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
+#
+#KNL_API_URL="http://eduvem-api-test-web.elasticbeanstalk.com/"
+#KNL_BUCKET="test.eduvem.com"
+
+KNL_API_URL="http://eduvem-api-prod-web.elasticbeanstalk.com/"
+KNL_BUCKET="eduvem.com"
+
+AWS_ACCESS_KEY_ID="AKIAJQP3BHOB4EVVTS4Q"
+AWS_SECRET_KEY="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"
+
+S3CMD_CFG=".s3cfg-kornell"
+KNL_CFG="./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/KornellConfig.js"
+
 echo Generating s3cmd config file
-echo "
-access_key = $AWS_ACCESS_KEY_ID
+echo -ne "access_key = $AWS_ACCESS_KEY_ID
 secret_key = $AWS_SECRET_KEY
-" > s3cfg
+" > $S3CMD_CFG
 
 echo Setting API Endpoint
-echo 'KornellConfig.apiEndpoint = "http://kornell-api-red.elasticbeanstalk.com";' > ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/KornellConfig.js
-echo Deploying to S3
+echo -n 'KornellConfig.apiEndpoint = "' > $KNL_CFG
+echo -n $KNL_API_URL >> $KNL_CFG
+echo -n '";' >> $KNL_CFG
+
+echo Deploying Cacheable Files to S3
 # Cacheable for 30 days (2592000 seconds)
-./tools/s3cmd/s3cmd -c ./s3cfg  \
+./tools/s3cmd/s3cmd -c $S3CMD_CFG  \
   --delete-removed \
   --exclude='WEB-INF/*' \
   --exclude='*.nocache.js' \
   --recursive \
   --add-header "Cache-control: max-age=2592000" \
-  sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://eduvem.com
+  sync ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/  s3://$KNL_BUCKET
 
+echo Deploying Non-cacheable Files to S3
 # Don't cache Kornell.nocache.js
-./tools/s3cmd/s3cmd -c ./s3cfg  \
+./tools/s3cmd/s3cmd -c $S3CMD_CFG  \
   --add-header "Cache-control: max-age=0" \
-  put ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/Kornell/Kornell.nocache.js  s3://eduvem.com/Kornell/
+  put ./kornell-gwt/target/kornell-gwt-0.0.1-SNAPSHOT/Kornell/Kornell.nocache.js  s3://$KNL_BUCKET/Kornell/
 
 echo Unless you see errors above, you should be deployed and good to go. 
