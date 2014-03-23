b813ec48ebba4dd7f04dc8be7511d5dbe53d3c47
Tiago Faria <tcfaria@gmail.com>
2014-03-23 01:23:29 -0400

    email test 2

--- kornell-api/src/main/scala/kornell/server/jdbc/repository/EventsRepo.scala
+++ kornell-api/src/main/scala/kornell/server/jdbc/repository/EventsRepo.scala
@@ -10,6 +10,7 @@ import kornell.core.event.EventFactory
 import kornell.server.jdbc.SQL.SQLHelper
 import kornell.server.util.EmailService
 import java.text.SimpleDateFormat
+import kornell.server.util.Settings
 
 
 object EventsRepo {
@@ -65,7 +66,7 @@ object EventsRepo {
 		
 	  if(EnrollmentState.preEnrolled.equals(toState) || EnrollmentState.enrolled.equals(toState)){
 	    val enrollment = EnrollmentRepo(enrollmentUUID).get
-	    if(enrollment.getPerson.getEmail != null && (System.getProperty("TEST_MODE") == null || !System.getProperty("TEST_MODE").equals("true"))){
+	    if(enrollment.getPerson.getEmail != null && !Settings.get("TEST_MODE").getOrElse("false").equals("true")){
 		    val courseClass = CourseClassesRepo(enrollment.getCourseClassUUID).get
 		    val course = CoursesRepo.byCourseClassUUID(courseClass.getUUID).get
 		    val institution = InstitutionsRepo.byUUID(courseClass.getInstitutionUUID).get
