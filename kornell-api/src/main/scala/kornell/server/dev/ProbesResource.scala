4add389d68ab221ecd6ea84cbbca5ea6aa9dc923
Julio Faerman <jfaerman@gmail.com>
2014-03-27 22:57:09 -0600

    Sending certificates to S3

--- kornell-api/src/main/scala/kornell/server/dev/ProbesResource.scala
+++ kornell-api/src/main/scala/kornell/server/dev/ProbesResource.scala
@@ -3,10 +3,40 @@ package kornell.server.dev
 import javax.ws.rs._
 import kornell.server.util.EmailSender
 import java.util.Date
+import java.io.ByteArrayInputStream
+import kornell.server.repository.s3.S3
+import kornell.core.util.UUID
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Path("/probes")
 @Produces(Array("text/plain"))
 class ProbesResource {
+  @GET
+  @Path("putToS3Sync")
+  def putToS3Sync = {
+    val bs = new ByteArrayInputStream("Some Nice Certificate Bytes ".getBytes())
+    val filename = UUID.random+".txt"
+    
+    S3.certificates.put(filename , 
+        bs,
+        "text-plain",
+        "Content-Disposition: attachment; filename=\"certificado.txt\"",
+        Map ( "certificatedata" -> "09/01/1980", "requestedby" -> "Fulano" ) )
+    
+   s"See your file at: /usercontent/certificates/$filename\n"
+  }
+  
+  
+  @GET
+  @Path("putToS3Async")
+  def putToS3Async = Future {
+    putToS3Sync
+  } onSuccess {
+    case s => println ("send an email or something...")
+  }
+  
+  
   @GET
   @Path("sendEmail")
   def sendEmail = {
--- kornell-api/src/main/scala/kornell/server/repository/s3/S3.scala
+++ kornell-api/src/main/scala/kornell/server/repository/s3/S3.scala
@@ -2,21 +2,21 @@ package kornell.server.repository.s3
 
 import java.io.ByteArrayInputStream
 import java.sql.ResultSet
-
 import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.collection.immutable.Stream.consWrapper
 import scala.io.Source
-
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectListing
-
-import kornell.core.util.StringUtils.composeURL
 import kornell.server.jdbc.SQL.SQLHelper
-
-//TODO: Change to HTTP
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider
+import kornell.server.util.Settings
+import com.amazonaws.services.s3.model.ObjectMetadata
+import scala.collection.JavaConverters._
+import java.io.InputStream
+import kornell.core.util.StringUtils._
 class S3(regionName: String,
   val accessKey: String,
   val secretKey: String,
@@ -28,7 +28,11 @@ class S3(regionName: String,
   type Actom = String
 
   val region = Region.getRegion(Regions.fromName(regionName))
-  val creds = new BasicAWSCredentials(accessKey, secretKey)
+  val creds = if(accessKey != null)
+    new BasicAWSCredentials(accessKey, secretKey)
+  else 
+	new ClasspathPropertiesFileCredentialsProvider().getCredentials()
+  
   val s3 = new AmazonS3Client(creds)
   lazy val client = s3
 
@@ -50,8 +54,16 @@ class S3(regionName: String,
   lazy val actoms: Stream[Actom] = keys.filter(isActom)
 
   def put(key: String, value: String) =
-    s3.putObject(bucket, prefix + "/" + key, new ByteArrayInputStream(value.getBytes()), null)
+    s3.putObject(bucket, composeURL(prefix,key), new ByteArrayInputStream(value.getBytes()), null)
 
+  def put (key:String, value:InputStream, contentType:String, contentDisposition:String, metadataMap:Map[String,String] ) = {
+    val metadata = new ObjectMetadata()
+    metadata.setUserMetadata(metadataMap asJava)
+    Option(contentType).foreach { metadata.setContentType(_) }
+    Option(contentDisposition).foreach { metadata.setContentDisposition(_) }
+    s3.putObject(bucket,composeURL(prefix,key), value, metadata)    
+  }
+    
   def getObject(key: String) =
     s3.getObject(bucket, prefix + "/" + key)
 
@@ -59,18 +71,20 @@ class S3(regionName: String,
 
   def url(ditributionPrefix: String, key: String) = composeURL(baseURL, prefix, ditributionPrefix, key)
 
-  //TODO: Resolve base url from region
-  //TODO: Stinking (set and/or get)
-  lazy val baseURL = {
-    if(distributionURL == null)
-    	"https://s3-" + regionName + ".amazonaws.com/" + bucket + "/"
-    else
-    	distributionURL
-  }
+  lazy val baseURL = distributionURL
+  
 
 }
 
 object S3 {
+  val DEFAULT_USER_CONTENT_BUCKET = "usercontent.test.eduvem.com";
+  val USER_CONTENT_BUCKET = Settings.get("USER_CONTENT_BUCKET").getOrElse(DEFAULT_USER_CONTENT_BUCKET)
+  
+  val certificates = new S3("sa-east-1",
+      null,null,
+      USER_CONTENT_BUCKET,
+      "userconent/certificates",null)
+  
   implicit def toS3(rs: ResultSet) = new S3(
     rs.getString("region"),
     rs.getString("accessKeyId"), rs.getString("secretAccessKey"),
