821da360d8547676b03f2d99a58971da8d301ee6
Julio Faerman <jfaerman@gmail.com>
2014-03-27 23:23:59 -0600

    Debug S3

--- kornell-api/src/main/scala/kornell/server/repository/s3/S3.scala
+++ kornell-api/src/main/scala/kornell/server/repository/s3/S3.scala
@@ -17,6 +17,8 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import scala.collection.JavaConverters._
 import java.io.InputStream
 import kornell.core.util.StringUtils._
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.auth.AWSCredentials
 class S3(regionName: String,
   val accessKey: String,
   val secretKey: String,
@@ -24,16 +26,21 @@ class S3(regionName: String,
   val prefix: String,
   distributionURL: String) {
 
-  //TODO: An actom is an undivisible unit of learning content. Pls write that on the wiki, bur for now it is just a key...
-  type Actom = String
+  
 
   val region = Region.getRegion(Regions.fromName(regionName))
-  val creds = if(accessKey != null)
-    new BasicAWSCredentials(accessKey, secretKey)
-  else 
-	new ClasspathPropertiesFileCredentialsProvider().getCredentials()
   
-  val s3 = new AmazonS3Client(creds)
+  //TODO: Option instead of null checks
+  val creds:AWSCredentials = if (accessKey != null)
+    new BasicAWSCredentials(accessKey, secretKey)
+  else if (getClass().getResourceAsStream("/AwsCredentials.properties") != null)
+    new ClasspathPropertiesFileCredentialsProvider().getCredentials()
+  else null
+
+  val s3 = if (creds != null)
+    new AmazonS3Client(creds)
+  else new AmazonS3Client()
+
   lazy val client = s3
 
   s3.setRegion(region)
@@ -51,19 +58,18 @@ class S3(regionName: String,
     .flatten(_.getObjectSummaries.asScala)
     .map(_.getKey)
 
-  lazy val actoms: Stream[Actom] = keys.filter(isActom)
 
   def put(key: String, value: String) =
-    s3.putObject(bucket, composeURL(prefix,key), new ByteArrayInputStream(value.getBytes()), null)
+    s3.putObject(bucket, composeURL(prefix, key), new ByteArrayInputStream(value.getBytes()), null)
 
-  def put (key:String, value:InputStream, contentType:String, contentDisposition:String, metadataMap:Map[String,String] ) = {
+  def put(key: String, value: InputStream, contentType: String, contentDisposition: String, metadataMap: Map[String, String]) = {
     val metadata = new ObjectMetadata()
     metadata.setUserMetadata(metadataMap asJava)
     Option(contentType).foreach { metadata.setContentType(_) }
     Option(contentDisposition).foreach { metadata.setContentDisposition(_) }
-    s3.putObject(bucket,composeURL(prefix,key), value, metadata)    
+    s3.putObject(bucket, composeURL(prefix, key), value, metadata)
   }
-    
+
   def getObject(key: String) =
     s3.getObject(bucket, prefix + "/" + key)
 
@@ -72,19 +78,18 @@ class S3(regionName: String,
   def url(ditributionPrefix: String, key: String) = composeURL(baseURL, prefix, ditributionPrefix, key)
 
   lazy val baseURL = distributionURL
-  
 
 }
 
 object S3 {
   val DEFAULT_USER_CONTENT_BUCKET = "usercontent.test.eduvem.com";
   val USER_CONTENT_BUCKET = Settings.get("USER_CONTENT_BUCKET").getOrElse(DEFAULT_USER_CONTENT_BUCKET)
-  
+
   val certificates = new S3("sa-east-1",
-      null,null,
-      USER_CONTENT_BUCKET,
-      "userconent/certificates",null)
-  
+    null, null,
+    USER_CONTENT_BUCKET,
+    "userconent/certificates", null)
+
   implicit def toS3(rs: ResultSet) = new S3(
     rs.getString("region"),
     rs.getString("accessKeyId"), rs.getString("secretAccessKey"),
