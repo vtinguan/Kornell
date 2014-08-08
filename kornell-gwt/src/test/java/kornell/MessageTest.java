de639b1b3f557db3a3540c74938054170626f61c
Tiago Faria <tcfaria@gmail.com>
2014-08-08 01:27:13 -0400

    msg test

---
+++ kornell-gwt/src/test/java/kornell/MessageTest.java
@@ -0,0 +1,17 @@
+package kornell;
+
+//import static org.easymock.EasyMock.*;
+import junit.framework.TestCase;
+import kornell.gui.client.presentation.message.MessagePlace;
+import kornell.gui.client.presentation.message.MessagePresenter;
+import kornell.gui.client.presentation.message.MessageView;
+
+import org.junit.Test;
+
+
+public class MessageTest extends TestCase {
+
+   @Test
+   public void testSendMessage() {
+  }
+}
