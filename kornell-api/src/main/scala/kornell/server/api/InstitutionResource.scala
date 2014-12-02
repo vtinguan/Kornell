882fc4106ef90b57088293e99bc3033813c66cbc
Gabriel Gravel <gravel.gabriel@gmail.com>
2014-12-02 00:52:51 -0500

    Added CRU API for Institution, moved acceptTerms to PersonResource

--- kornell-api/src/main/scala/kornell/server/api/InstitutionResource.scala
+++ kornell-api/src/main/scala/kornell/server/api/InstitutionResource.scala
@@ -1,43 +1,49 @@
 package kornell.server.api
 
-import javax.ws.rs.Produces
 import javax.ws.rs.Consumes
-import javax.ws.rs.core.SecurityContext
-import kornell.core.entity.Institution
-import javax.ws.rs.PUT
 import javax.ws.rs.GET
-import kornell.server.jdbc.repository.InstitutionsRepo
-import kornell.server.jdbc.repository.AuthRepo
+import javax.ws.rs.POST
+import javax.ws.rs.PUT
 import javax.ws.rs.Path
-import javax.ws.rs.core.Context
-import kornell.server.jdbc.repository.PersonRepo
+import javax.ws.rs.Produces
+import kornell.core.entity.Institution
+import kornell.server.jdbc.repository.InstitutionsRepo
+import kornell.server.util.Conditional.toConditional
+import kornell.server.util.RequirementNotMet
 import kornell.core.to.InstitutionRegistrationPrefixesTO
 
 
 @Produces(Array(Institution.TYPE))
 class InstitutionResource(uuid: String) {
+  def this() = this(null)
+  
   @GET
-  def get = InstitutionsRepo.byUUID(uuid)
+  def get =  {
+    InstitutionsRepo.byUUID(uuid)
+   }.requiring(isPlatformAdmin, RequirementNotMet).get
   
   @PUT
-  @Produces(Array("text/plain"))
-  @Path("acceptTerms")
-  def acceptTerms(implicit @Context sc: SecurityContext) = AuthRepo().withPerson{ p =>
-    PersonRepo(p.getUUID).acceptTerms
-  }
-  
-  @PUT
-  @Produces(Array("text/plain"))
   @Consumes(Array(Institution.TYPE))
-  def update(implicit @Context sc: SecurityContext, institution: Institution) = AuthRepo().withPerson{ p =>
+  def update(institution: Institution) = {
     InstitutionsRepo.update(institution)
-  }
+  }.requiring(isPlatformAdmin, RequirementNotMet).get
+  
+  @POST
+  @Consumes(Array(Institution.TYPE))
+  def create(institution: Institution) = {
+    InstitutionsRepo.create(institution)
+  }.requiring(isPlatformAdmin, RequirementNotMet).get
   
   @GET
   @Produces(Array(InstitutionRegistrationPrefixesTO.TYPE))
   @Path("registrationPrefixes")
-  def getRegistrationPrefixes(implicit @Context sc: SecurityContext) = AuthRepo().withPerson{ p =>
+  def getRegistrationPrefixes() = {
     InstitutionsRepo.getRegistrationPrefixes(uuid)
-  }
+  }.requiring(isPlatformAdmin, RequirementNotMet).get
   
+}
+
+object InstitutionResource {
+    def apply() = new InstitutionResource()
+    def apply(uuid: String) = new InstitutionResource(uuid)
 }
---
+++ kornell-api/src/test/scala/kornell/server/api/InstitutionSpec.scala
@@ -0,0 +1,122 @@
+package kornell.server.api
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import kornell.server.test.UnitSpec
+import kornell.server.helper.GenPlatformAdmin
+import kornell.server.repository.Entities
+import kornell.server.helper.GenInstitutionAdmin
+import kornell.server.util.RequirementNotMet
+
+@RunWith(classOf[JUnitRunner])
+class InstitutionSpec extends UnitSpec 
+    with GenPlatformAdmin
+    with GenInstitutionAdmin {
+
+  "The platformAdmin" should 
+  "be able to create a new institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+  }
+  
+  "The institutionAdmin" should 
+  "not be able to create a new institution" in asInstitutionAdmin {
+    try {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+    } catch {
+      case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+      case default:Throwable => fail() 
+    }
+  }
+  
+  "A person" should 
+  "not be able to create a new institution" in asPerson {
+    try {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+    } catch {
+      case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+      case default:Throwable => fail() 
+    }
+  }
+  
+  "The platformAdmin" should 
+  "be able to modify an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+    newInstitution.setFullName("test");
+    val modifiedInstitution = InstitutionResource().update(newInstitution)
+    assert("test" == modifiedInstitution.getFullName)
+  }
+  
+  "The institutionAdmin" should 
+  "not be able to modify an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+    newInstitution.setFullName("test");
+    asInstitutionAdmin {
+      try {
+        val modifiedInstitution = InstitutionResource().update(newInstitution)
+      } catch {
+        case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+        case default:Throwable => fail() 
+      }
+    }
+  }
+  
+  "A person" should 
+  "not be able to modify an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+    newInstitution.setFullName("test");
+    asPerson {
+      try {
+        val modifiedInstitution = InstitutionResource().update(newInstitution)
+      } catch {
+        case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+        case default:Throwable => fail() 
+      }
+    }
+  }
+  
+  "The platformAdmin" should 
+  "be able to get an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+
+    val fetchedInstitution = InstitutionResource(newInstitution.getUUID).get
+    assert(newInstitution.getFullName == fetchedInstitution.get.getFullName)
+    
+  }
+  
+  "The institutionAdmin" should 
+  "not be able to get an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+
+    asInstitutionAdmin{
+        try {
+            val fetchedInstitution = InstitutionResource(newInstitution.getUUID).get
+        } catch {
+          case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+          case default:Throwable => fail() 
+        }
+    }
+  }
+  
+  "The person" should 
+  "not be able to get an institution" in asPlatformAdmin {
+    val newInstitution = InstitutionResource().create( 
+        Entities.newInstitution(randUUID, randStr, randStr, randStr, randURL, randURL, false, false, false, false, null, ""))
+
+    asPerson{
+        try {
+            val fetchedInstitution = InstitutionResource(newInstitution.getUUID).get
+        } catch {
+          case ise:IllegalStateException => assert(ise.getCause.eq(RequirementNotMet))
+          case default:Throwable => fail() 
+        }
+    }
+  }
+}
