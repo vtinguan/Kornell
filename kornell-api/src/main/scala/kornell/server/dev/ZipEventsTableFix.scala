2e60a5d212a312fae5f66c6603b8e2f1f3759d82
Julio Faerman <jfaerman@gmail.com>
2015-03-18 23:29:22 -0300

    Better progress reporting on fix

--- kornell-api/src/main/scala/kornell/server/dev/ZipEventsTableFix.scala
+++ kornell-api/src/main/scala/kornell/server/dev/ZipEventsTableFix.scala
@@ -5,43 +5,54 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import kornell.server.jdbc.DataSources
 import java.sql.Connection
+import java.util.Date
+import java.util.concurrent.atomic.AtomicInteger
+import scala.util.Try
 
+//HOWTO: mvn exec:java -Dexec.mainClass=kornell.server.dev.ZipEventsTableFix
 object ZipEventsTableFix extends App {
 
+  val t0 = System.currentTimeMillis
   val df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS")
-  
-  def zipEntry(enrollmentUUID: String, actomKey: String, entryKey: String) = {
-     val conn = DataSources.connectionFactory.get()
-     val stmt = conn.prepareStatement("delete from ActomEntryChangedEvent where uuid=?")
-     var reds = 0
-     var last_value:String = null
-	 val entries = sql"""
+
+  def zipEntry(conn: Connection,
+    enrollmentUUID: String,
+    actomKey: String,
+    entryKey: String) = {
+    val stmt = conn.prepareStatement(
+        "delete from ActomEntryChangedEvent where uuid=?")
+    var reds = 0
+    var last_value: String = null
+    val entries = sql"""
 	  select *
 	  from ActomEntryChangedEvent
 	  where enrollment_uuid=$enrollmentUUID
 	   and actomKey=$actomKey
 	   and entryKey=$entryKey
 	  order by ingestedAt
-    """.foreach { rs => 
-       count += 1
-       val uuid = rs.getString("uuid");
-       val entryKey = rs.getString("entryValue")
-       val entryValue = rs.getString("entryValue")
-       val entryTime  = rs.getTimestamp("ingestedAt")
-       val entryTimeS = df.format(entryTime)
-       val isRedundant = (entryValue == last_value)
-       if (isRedundant) {
-         reds += 1
-         stmt.setString(1, uuid)
-         stmt.addBatch()
-       }
-       //println(s"[${if(isRedundant)"R"else"XXX";}][$entryTimeS][$entryKey]=[$entryValue]")
-       last_value = entryValue
-     }
-     println(s"*** Removing [$reds] redundant records from ActomEntryChangedEvent [$enrollmentUUID, $actomKey, $entryKey]")
-     if (reds > 0) stmt.executeUpdate     
-     stmt.close
-     conn.close
+    """.foreach { rs =>
+      count.incrementAndGet
+      val uuid = rs.getString("uuid");
+      val entryKey = rs.getString("entryValue")
+      val entryValue = rs.getString("entryValue")
+      val entryTime = rs.getTimestamp("ingestedAt")
+      val entryTimeS = Try {df.format(entryTime)}.getOrElse("unparseable_date@"+System.currentTimeMillis)
+      val isRedundant = (entryValue == last_value)
+      if (isRedundant) {
+        reds += 1
+        stmt.setString(1, uuid)
+        stmt.addBatch()
+      }
+      //println(s"[${if(isRedundant)"R"else"XXX";}][$entryTimeS][$entryKey]=[$entryValue]")
+      last_value = entryValue
+    }
+    if (reds > 0) {
+      fixed.addAndGet(reds)
+      println(s"*** Removing [$reds] redundant records from ActomEntryChangedEvent [$enrollmentUUID, $actomKey, $entryKey]")
+      stmt.executeBatch
+    }
+    stmt.close
+
   }
 
   def zipActom(enrollmentUUID: String, actomKey: String) = {
@@ -52,12 +63,28 @@ object ZipEventsTableFix extends App {
 	 where enrollment_uuid=$enrollmentUUID
 	   and actomKey=$actomKey
 	 """.map[String]
-    entryKeys foreach { key => zipEntry(enrollmentUUID, actomKey, key) }
+    entryKeys foreach { key =>
+      val conn = DataSources.connectionFactory.get()
+      try {
+        conn.setAutoCommit(false)
+        zipEntry(conn, enrollmentUUID, actomKey, key)
+        conn.commit
+      } finally {
+        conn.rollback
+        conn.close
+      }
+    }
   }
 
   def zipEnrollment(enrollmentUUID: String) = {
     println(s"** Zipping [${enrollmentUUID}]")
-    println(s"** Count [${count}]")
+    val t1 = System.currentTimeMillis
+    println(s"** Now [${df.format(new Date(t1))}]")
+    val elapsed = (t1 - t0) / 1000
+    println(s"** Elaspsed [${elapsed}]")
+    println(s"** Count [${count}] rs")
+    val pace = count.get / elapsed
+    println(s"** Pace  [$pace] rs/s")
     val actoms = sql"""
     select distinct(actomKey)
     from ActomEntryChangedEvent
@@ -66,11 +93,24 @@ object ZipEventsTableFix extends App {
     actoms foreach { actomKey => zipActom(enrollmentUUID, actomKey) }
   }
 
-  var count = 0
+  println(s"==== BEGIN ====")
+  val mainThread = Thread.currentThread();
+  var count = new AtomicInteger
+  var fixed = new AtomicInteger
   val enrollments = sql"""select distinct(enrollment_uuid)
   	from ActomEntryChangedEvent""".map[String]
   println(s"* Zipping ${enrollments.size} enrollments")
   //God bless parallel colletions #scala
   enrollments.par foreach zipEnrollment
-
+  scala.sys.addShutdownHook {
+    println(s"==== END ====")
+    println(s"** Then [${df.format(new Date(t0))}]")
+    println(s"** Now  [${df.format(new Date)}]")
+    println(s"** Count [${count}] rs")
+    val t1 = System.currentTimeMillis
+    val pace = ((1000) * count.get) / (t1 - t0)
+    println(s"** Pace  [$pace] rs/s")
+    mainThread.join
+  }
+  println(s"==== === ====")
 }
