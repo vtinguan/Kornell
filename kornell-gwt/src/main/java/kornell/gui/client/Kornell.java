ac5122ff9c80eaf6125c961bba10cac1dc6b87de
Julio Faerman <jfaerman@gmail.com>
2015-08-16 22:47:12 -0300

    Prevent client logging configuration from getting into servers.

--- kornell-gwt/src/main/java/kornell/gui/client/Kornell.java
+++ kornell-gwt/src/main/java/kornell/gui/client/Kornell.java
@@ -6,31 +6,46 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.logging.client.LogConfiguration;
 
 public class Kornell implements EntryPoint {
-	
+
 	Logger logger = Logger.getLogger(Kornell.class.getName());
 	ClientFactory clientFactory = GWT.create(ClientFactory.class);
-	
+
 	@Override
 	public void onModuleLoad() {
+		loggerSoundcheck();
 		final long t0 = System.currentTimeMillis();
-    Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-        @Override
-        public void execute() {
-      		startLMS();
-      		long t1 = System.currentTimeMillis();
-      		logger.info("Kornell GWT started in ["+(t1-t0)+" ms]" );
-        }
-    });
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				startLMS();
+				long t1 = System.currentTimeMillis();
+				logger.info("Kornell GWT started in [" + (t1 - t0) + " ms]");
+			}
+		});
+	}
+
+	private void loggerSoundcheck() {
+		if(LogConfiguration.loggingIsEnabled()){
+			String msg = "Hello from Kornell Client - ";
+			logger.finest(msg + "FINEST");
+			logger.finer(msg + "FINER");
+			logger.fine(msg + "FINE");
+			logger.info( "INFO");
+			logger.warning(msg + "WARNING");
+			logger.severe(msg + "SEVERE");
+		}
+		
 	}
 
 	private void startLMS() {
 		clientFactory.startApp();
 		clientFactory.logState();
 	}
-	
-	public ClientFactory getClientFactory(){
+
+	public ClientFactory getClientFactory() {
 		return clientFactory;
 	}
 
