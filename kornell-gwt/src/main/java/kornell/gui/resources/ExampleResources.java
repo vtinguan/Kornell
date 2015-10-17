d2834db4d4641f7ab7d41d479bb97acab8f415e5
Tiago Faria <tcfaria@gmail.com>
2015-10-17 00:48:35 -0400

    delete unused classes

--- kornell-gwt/src/main/java/kornell/gui/resources/ExampleResources.java
+++
@@ -1,8 +0,0 @@
-package kornell.gui.resources;
-
-import com.github.gwtbootstrap.client.ui.resources.Resources;
-
-public interface ExampleResources extends Resources {
-  //@Source("css/bootstrap.min.css")
-  //TextResource bootstrapCss();
-} 
