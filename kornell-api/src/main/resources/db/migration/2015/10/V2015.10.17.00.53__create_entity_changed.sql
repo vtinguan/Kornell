970ff6c495126ace7f4cd552aa8076fefa04c46b
Julio Faerman <jfaerman@gmail.com>
2015-10-23 20:32:28 -0200

    Fixed bad pasting

--- kornell-api/src/main/resources/db/migration/2015/10/V2015.10.17.00.53__create_entity_changed.sql
+++ kornell-api/src/main/resources/db/migration/2015/10/V2015.10.17.00.53__create_entity_changed.sql
@@ -1,6 +1,13 @@
-CREATE TABLE ContentRepository (
+DROP TABLE IF EXISTS `EntityChanged`;
+CREATE TABLE IF NOT EXISTS `EntityChanged` (
   `uuid` char(36) NOT NULL,
-  `repositoryType` varchar(255) NOT NULL, 
-  PRIMARY KEY (`uuid`)
-) AS SELECT uuid, 'S3' as repositoryType from S3ContentRepository;
-
+  `eventFiredAt` char(29) DEFAULT NULL,
+  `personUUID` char(36),
+  `entityType` char(36) NOT NULL DEFAULT 'FIXME-PLS',
+  `institutionUUID` char(36) NOT NULL,
+  `entityUUID` char(36) NOT NULL,
+  `fromValue` text,
+  `toValue` text,
+  PRIMARY KEY (`uuid`),
+  KEY `fk_entitychanged_person_personUUID_idx` (`personUUID`)
+);
