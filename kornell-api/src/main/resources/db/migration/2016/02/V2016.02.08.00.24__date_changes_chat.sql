6b44abfb6fe77a296a44379ec78255bbce953de0
Gabriel Gravel <gravel.gabriel@gmail.com>
2016-02-08 01:04:02 -0500

    Added pattern to date cleanup flyways

--- kornell-api/src/main/resources/db/migration/2016/02/V2016.02.08.00.24__date_changes_chat.sql
+++ kornell-api/src/main/resources/db/migration/2016/02/V2016.02.08.00.24__date_changes_chat.sql
@@ -1,7 +1,7 @@
-update ChatThread set createdAt = replace(replace(replace(replace(replace(createdAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), 'Z', '');
-update ChatThreadMessage set sentAt = replace(replace(replace(replace(replace(sentAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), 'Z', '');
-update ChatThreadParticipant set lastReadAt = replace(replace(replace(replace(replace(lastReadAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), 'Z', '');
-update ChatThreadParticipant set lastJoinDate = replace(replace(replace(replace(replace(lastJoinDate, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), 'Z', '');
+update ChatThread set createdAt = replace(replace(replace(replace(replace(replace(createdAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update ChatThreadMessage set sentAt = replace(replace(replace(replace(replace(replace(sentAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update ChatThreadParticipant set lastReadAt = replace(replace(replace(replace(replace(replace(lastReadAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update ChatThreadParticipant set lastJoinDate = replace(replace(replace(replace(replace(replace(lastJoinDate, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
 
 alter table ChatThread modify createdAt timestamp null default null;
 alter table ChatThreadMessage modify sentAt timestamp null default null;
