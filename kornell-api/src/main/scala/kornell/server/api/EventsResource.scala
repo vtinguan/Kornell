050e9e3d74dcedb803f41ea907365440eb643204
Gabriel Gravel <gravel.gabriel@gmail.com>
2016-02-25 01:38:46 -0500

    Added permissions on event calls

--- kornell-api/src/main/scala/kornell/server/api/EventsResource.scala
+++ kornell-api/src/main/scala/kornell/server/api/EventsResource.scala
@@ -19,44 +19,53 @@ import kornell.core.event.EnrollmentTransferred
 import kornell.core.event.EntityChanged
 import kornell.core.entity.AuditedEntityType
 import kornell.core.to.EntityChangedEventsTO
-//TODO: Why are these returning Unit?
+import kornell.server.util.Conditional.toConditional
+import kornell.server.util.AccessDeniedErr
+import kornell.server.jdbc.repository.EnrollmentRepo
+import javax.ws.rs.core.Response
+
 @Path("events")
 class EventsResource {
   
   @PUT 
   @Path("actomEntered")
   @Consumes(Array(ActomEntered.TYPE))
-  def putActomEntered(event:ActomEntered){
-	 EventsRepo.logActomEntered(event)	
-  }
+  def putActomEntered(event:ActomEntered):Response = {
+	 EventsRepo.logActomEntered(event)
+	 Response.noContent().build()
+  }.requiring(EnrollmentRepo(event.getEnrollmentUUID).get.getPersonUUID == getAuthenticatedPersonUUID, AccessDeniedErr()).get
   
   @PUT
   @Path("enrollmentStateChanged")
   @Consumes(Array(EnrollmentStateChanged.TYPE))
-  def putEnrollmentStateChanged(event:EnrollmentStateChanged){
+  def putEnrollmentStateChanged(event:EnrollmentStateChanged):Response = {
      EventsRepo.logEnrollmentStateChanged(event)
-  }
+     Response.noContent().build()
+  }.requiring(event.getFromPersonUUID == getAuthenticatedPersonUUID, AccessDeniedErr()).get
   
   @PUT
   @Path("courseClassStateChanged")
   @Consumes(Array(CourseClassStateChanged.TYPE))
-  def putCourseClassStateChanged(event:CourseClassStateChanged){
+  def putCourseClassStateChanged(event:CourseClassStateChanged):Response = {
      EventsRepo.logCourseClassStateChanged(event)
-  }
+     Response.noContent().build()
+  }.requiring(event.getFromPersonUUID == getAuthenticatedPersonUUID, AccessDeniedErr()).get
   
   @PUT
   @Path("attendanceSheetSigned")
   @Consumes(Array(AttendanceSheetSigned.TYPE))
-  def putAttendanceSheetSigned(event:AttendanceSheetSigned){
+  def putAttendanceSheetSigned(event:AttendanceSheetSigned):Response = {
      EventsRepo.logAttendanceSheetSigned(event)
-  }
+     Response.noContent().build()
+  }.requiring(event.getPersonUUID == getAuthenticatedPersonUUID, AccessDeniedErr()).get
   
   @PUT
   @Path("enrollmentTransferred")
   @Consumes(Array(EnrollmentTransferred.TYPE))
-  def putEnrollmentTransferred(event:EnrollmentTransferred) = {
+  def putEnrollmentTransferred(event:EnrollmentTransferred):Response = {
      EventsRepo.logEnrollmentTransferred(event)
-  }
+     Response.noContent().build()
+  }.requiring(event.getFromPersonUUID == getAuthenticatedPersonUUID, AccessDeniedErr()).get
   
   @GET
   @Produces(Array(EntityChangedEventsTO.TYPE))
