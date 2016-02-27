265f352337ec3626e2e0c6f5db2b367c200a715b
Tiago Faria <tcfaria@gmail.com>
2016-02-27 17:26:39 -0500

    Revert "remove migrations that timeout to deploy on prod"
    This reverts commit a901ecf9595ef16092ca593961fc7b1cbc52edbd.

---
+++ kornell-api/src/main/resources/db/migration/2016/02/V2016.02.08.00.22__events_date_changes.sql
@@ -0,0 +1,14 @@
+-- cleanup script
+update ActomEntered set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update AttendanceSheetSigned set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update EnrollmentStateChanged set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update EnrollmentTransferred set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update EntityChanged set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+update CourseClassStateChanged set eventFiredAt = replace(replace(replace(replace(replace(replace(eventFiredAt, '-02:00', ''), '-03:00', ''), '-04:00', ''), '-05:00', ''), '+00:00', ''), 'Z', '');
+
+alter table ActomEntered modify eventFiredAt timestamp null default null;
+alter table AttendanceSheetSigned modify eventFiredAt timestamp null default null;
+alter table EnrollmentStateChanged modify eventFiredAt timestamp null default null;
+alter table EnrollmentTransferred modify eventFiredAt timestamp null default null;
+alter table EntityChanged modify eventFiredAt timestamp null default null;
+alter table CourseClassStateChanged modify eventFiredAt timestamp null default null;
