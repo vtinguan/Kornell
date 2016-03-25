266e12d08baa08f34dddf238a3553021c1233a24
Julio Faerman <jfaerman@gmail.com>
2016-03-25 17:11:34 -0300

    Add Hikari CP

--- kornell-api/src/main/scala/kornell/server/jdbc/DataSources.scala
+++ kornell-api/src/main/scala/kornell/server/jdbc/DataSources.scala
@@ -11,20 +11,22 @@ import scala.util.Success
 import scala.util.Failure
 import SQL._
 import kornell.server.util.Settings._
+import com.zaxxer.hikari.HikariDataSource
+import com.googlecode.flyway.core.Flyway
 
-object DataSources { 
+object DataSources {
   val log = Logger.getLogger(getClass.getName)
 
-  def ping(cf: ConnectionFactory,dbDesc:String=""): Try[ConnectionFactory] = Try {
+  def ping(cf: ConnectionFactory, dbDesc: String = ""): Try[ConnectionFactory] = Try {
     log.info(s"Pinging database [$dbDesc]")
-    try{
+    try {
       val conn = cf()
       val stmt = conn.createStatement
       stmt.execute("select 40+2")
       stmt.close
-      conn.close    
+      conn.close
       cf
-    }catch {
+    } catch {
       case t: Throwable => {
         log.info("Could not connect to [$dbDesc]")
         t.printStackTrace() // TODO: handle error
@@ -33,7 +35,7 @@ object DataSources {
     }
   }
 
-  lazy val KornellDS = {
+  lazy val kornellDS = {
     val context = new InitialContext()
       .lookup(JNDI_ROOT)
       .asInstanceOf[Context]
@@ -42,29 +44,44 @@ object DataSources {
     ds
   }
 
-  lazy val JNDI: Try[ConnectionFactory] =    
-    ping ({ () => KornellDS.getConnection }, s"$JNDI_ROOT/$JNDI_DATASOURCE")
-  
+  lazy val hikariDS = {
+    val ds = new HikariDataSource();
+    ds.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource")
+    ds.addDataSourceProperty("dataSource.url", prop("JDBC_CONNECTION_STRING"))
+    ds.addDataSourceProperty("dataSource.user", prop("JDBC_USERNAME"))
+    ds.addDataSourceProperty("dataSource.password", prop("JDBC_PASSWORD"))
+    ds.addDataSourceProperty("dataSource.databaseName", "ebdb")
+    ds
+  }
 
-  def getConnection(url: String, user: String, pass: String): Try[ConnectionFactory] =     
-    ping ({ () => DriverManager.getConnection(url, user, pass) },s"JDBC@$url,$user,$pass")
-  
+  lazy val JNDI: Try[ConnectionFactory] =
+    ping({ () => kornellDS.getConnection }, s"$JNDI_ROOT/$JNDI_DATASOURCE")
 
-  lazy val LOCAL = getConnection(DEFAULT_URL,DEFAULT_USERNAME,DEFAULT_PASSWORD)
+  def getConnection(url: String, user: String, pass: String): Try[ConnectionFactory] =
+    ping({ () => DriverManager.getConnection(url, user, pass) }, s"JDBC@$url,$user,$pass")
 
+  lazy val LOCAL = getConnection(DEFAULT_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD)
 
   lazy val SYSPROPS = getConnection(
     prop("JDBC_CONNECTION_STRING"),
     prop("JDBC_USERNAME"),
     prop("JDBC_PASSWORD"))
 
-  //TODO: Consider configuring from imported implicit  (like ExecutionContext)
-  val connectionFactory = JNDI.orElse(SYSPROPS).orElse(LOCAL)
+  lazy val POOL = ping ({ () => hikariDS.getConnection  },"HikariDS")
+
+  val connectionFactory = POOL orElse JNDI orElse SYSPROPS orElse LOCAL 
   
+  def configure(flyway:Flyway) = connectionFactory match {
+      case JNDI => flyway.setDataSource(kornellDS)
+      case _ => flyway.setDataSource(
+          prop("JDBC_CONNECTION_STRING"),
+          prop("JDBC_USERNAME"),
+          prop("JDBC_PASSWORD")) 
+  }
+
   connectionFactory match {
     case Success(cf) => log.info(s"Connection Factory validated ${connectionFactory} . Nice!");
     case Failure(e) => log.severe("Can't live without a databse, sorry :("); //TODO: DIE
   }
-  
 
 }
--- kornell-api/src/main/scala/kornell/server/jdbc/Migration.scala
+++ kornell-api/src/main/scala/kornell/server/jdbc/Migration.scala
@@ -22,28 +22,11 @@ object Migration {
     println("[SYSOUT] Starting Database Migration")
     log.info("[INFO] Starting Database Migration")
     val flyway = new Flyway()
-    setDataSource(flyway)
+    DataSources.configure(flyway)
     flyway.setLocations("db/jdbcmigration","db/migration") 
     flyway.setOutOfOrder(true)
     flyway.migrate()
   }
 
-  //TODO: Clean this ugly hack moving it to a "DataSources" method
-  private def setDataSource(flyway: com.googlecode.flyway.core.Flyway): Unit =
-    DataSources.JNDI match {
-      case Success(_) => {
-        log.info("Using JNDI as a target for migrations")
-        flyway.setDataSource(DataSources.KornellDS)
-      }
-      case Failure(_) => {
-        log.info("Using LOCAL as a target for migration")
-        DataSources.SYSPROPS match {
-          case Success(_) => flyway.setDataSource(
-            prop("JDBC_CONNECTION_STRING"),
-            prop("JDBC_USERNAME"),
-            prop("JDBC_PASSWORD"))
-          case Failure(_) => flyway.setDataSource(DEFAULT_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD)
-        }
-      }
-    }
+  
 }
