d441846cc96eae05c4cf085e0c3a0f1b8508ce53
Julio Faerman <jfaerman@gmail.com>
2016-04-23 00:40:20 -0300

    aws eb fixes

--- cfn-eb-java.sh
+++ cfn-eb-java.sh
@@ -7,8 +7,9 @@ ARG_URL="ParameterKey=knljdbcurl,ParameterValue=\"$JDBC_CONNECTION_STRING\""
 ARG_DRIVER="ParameterKey=knljdbcdriver,ParameterValue=$JDBC_DRIVER"
 ARG_USERNAME="ParameterKey=knljdbcusername,ParameterValue=$JDBC_USERNAME"
 ARG_PASSWORD="ParameterKey=knljdbcpassword,ParameterValue=$JDBC_PASSWORD"
+ARG_DB_VPC="ParameterKey=knldbvpcid,ParameterValue=$DB_VPC_ID"
 
 
-CFN_CREATE_ARGS="--parameters  $ARG_URL $ARG_DRIVER $ARG_USERNAME $ARG_PASSWORD"
+CFN_CREATE_ARGS="--parameters  $ARG_URL $ARG_DRIVER $ARG_USERNAME $ARG_PASSWORD $ARG_DB_VPC"
 
 source cfn-create-stack.sh
