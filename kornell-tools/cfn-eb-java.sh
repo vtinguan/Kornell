e4716e11314d7ecaa8e783124e6d087c6209f9a7
Julio Faerman <jfaerman@gmail.com>
2016-05-01 11:37:18 -0300

    Added SMTP API parameters

--- kornell-tools/cfn-eb-java.sh
+++ kornell-tools/cfn-eb-java.sh
@@ -14,6 +14,8 @@ demmand "VPC_API_SECGS"
 demmand "API_KEYPAIR"
 demmand "CERT_ARN"
 
+
+
 TIER="eb"
 BRANCH=${BRANCH:-"master"}
 
@@ -21,6 +23,13 @@ ARG_URL="ParameterKey=knljdbcurl,ParameterValue='$JDBC_CONNECTION_STRING'"
 ARG_DRIVER="ParameterKey=knljdbcdriver,ParameterValue=$JDBC_DRIVER"
 ARG_USERNAME="ParameterKey=knljdbcusername,ParameterValue=$JDBC_USERNAME"
 ARG_PASSWORD="ParameterKey=knljdbcpassword,ParameterValue=$JDBC_PASSWORD"
+
+ARG_SMTP_HOST="ParameterKey=knlsmtphost,ParameterValue='$SMTP_HOST'"
+ARG_SMTP_PORT="ParameterKey=knlsmtpport,ParameterValue='$SMTP_HOST'"
+ARG_SMTP_USERNAME="ParameterKey=knlsmtpusername,ParameterValue='$SMTP_USERNAME'"
+ARG_SMTP_PASSWORD="ParameterKey=knlsmtppassword,ParameterValue='$SMTP_PASSWORD'"
+ARG_REPLY_TO="ParameterKey=knlreplyto,ParameterValue='$REPLY_TO'"
+ARG_USER_CONTENT_BUCKET="ParameterKey=knlusercontentbucketname,ParameterValue='$USER_CONTENT_BUCKET'"
 
 ARG_VPC_ID="ParameterKey=knlvpc,ParameterValue='$VPC_ID'"
 ARG_VPC_API_SUBNETS="ParameterKey=knlapisubnetids,ParameterValue='$VPC_API_SUBNETS'"
@@ -30,5 +39,6 @@ ARG_API_KEYPAIR="ParameterKey=knlkeypair,ParameterValue='$API_KEYPAIR'"
 ARG_CERT_ARN="ParameterKey=knlcertificateid,ParameterValue='$CERT_ARN'"
 
 CFN_CREATE_ARGS="--parameters $ARG_VPC_ID $ARG_URL $ARG_DRIVER $ARG_USERNAME $ARG_PASSWORD $ARG_VPC_API_SUBNETS $ARG_VPC_API_SECGS $ARG_API_KEYPAIR $ARG_API_BRANCH $ARG_CERT_ARN"
+CFN_CREATE_ARGS="$CFN_CREATE_ARGS $ARG_SMTP_HOST $ARG_SMTP_PORT $ARG_SMTP_USERNAME $ARG_SMTP_PASSWORD $ARG_REPLY_TO $ARG_USER_CONTENT_BUCKET"
 
 source $DIR/cfn-create-stack.sh
