b95f36d7fe8d08c3206d9fd8c539c9e651189197
Julio Faerman <jfaerman@gmail.com>
2016-05-01 23:33:08 -0300

    default to bamboo aws credentials

--- kornell-tools/src/main/deploy/deploy-gwt.sh
+++ kornell-tools/src/main/deploy/deploy-gwt.sh
@@ -3,6 +3,8 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../bash/bash-utils.sh
 
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-"$bamboo_awsAccessKeyId"} 
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-"$bamboo_awsSecretKeypassword"}
 TEMP_DIR=${TEMP_DIR:-"$DIR/kornell-gwt-s3/"}
 REGION=${bamboo_REGION:-"us-east-1"}
 GWT_ARTIFACT=${bamboo_GWT_ARTIFACT:-"$DIR/../kornell-gwt/target/kornell-gwt-s3.zip"}
@@ -18,6 +20,7 @@ log "Uncompressing [$GWT_ARTIFACT] to [$TEMP_DIR]"
 mkdir -p $TEMP_DIR
 unzip -o $GWT_ARTIFACT -d $TEMP_DIR
 
+log "Using access key id [$AWS_ACCESS_KEY_ID]"
 log "Deploying cacheable files to S3"
 aws s3 sync \
   $TEMP_DIR \
