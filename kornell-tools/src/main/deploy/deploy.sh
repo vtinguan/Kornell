18e49e316d9ef0d0ce43b576412f02ef1918310c
Julio Faerman <jfaerman@gmail.com>
2016-05-02 00:31:10 -0300

    Fixing deployment

--- kornell-tools/src/main/deploy/deploy.sh
+++ kornell-tools/src/main/deploy/deploy.sh
@@ -3,12 +3,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../bash/bash-utils.sh
 
-if [ -n "$bamboo_awsAccessKeyId" ]; then
-	export AWS_ACCESS_KEY_ID=$bamboo_awsAccessKeyId
-	export AWS_SECRET_ACCESS_KEY=$bamboo_awsAccessKeyId	
-	echo "Using access key id [$AWS_ACCESS_KEY_ID]"
-fi 
-
 source $DIR/deploy-gwt.sh
 source $DIR/deploy-api.sh
 
