a49ad4e466c53d68460a7079704674ea12569732
Julio Faerman <jfaerman@gmail.com>
2016-05-02 00:13:08 -0300

    Fixing deployment

--- kornell-tools/src/main/deploy/deploy.sh
+++ kornell-tools/src/main/deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../bash/bash-utils.sh
 
 if [ -n "$bamboo_awsAccessKeyId" ]; then
 	export AWS_ACCESS_KEY_ID=$bamboo_awsAccessKeyId
