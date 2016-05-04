93ff8950511bca90dad899bc863f5c6f9b1a9db6
Julio Faerman <jfaerman@gmail.com>
2016-05-04 17:37:01 -0300

    Minor deployment improvements

--- kornell-tools/src/main/cfn/cfn-cloudfront.sh
+++ kornell-tools/src/main/cfn/cfn-cloudfront.sh
@@ -12,7 +12,6 @@ STACK_NAME=${STACK_NAME:-"cfn-cloudfront-$PURPOSE-$DOMAIN_PREFIX"}
 demmand "DOMAIN_NAME"
 demmand "S3_GWT_BUCKET"
 demmand "S3_LOGS_BUCKET"
-demmand "S3_REPOSITORY_BUCKET"
 demmand "S3_USERCONTENT_BUCKET"
 demmand "API_ENDPOINT"
 demmand "CERT_ID"
@@ -22,13 +21,12 @@ demmand "INSTITUTION"
 ARG_S3_DOMAIN_NAME="ParameterKey=knldomainname,ParameterValue='$DOMAIN_NAME'"
 ARG_S3_GWT_BUCKET="ParameterKey=knlgwtbucketname,ParameterValue='$S3_GWT_BUCKET'"
 ARG_S3_LOGS_BUCKET="ParameterKey=knllogsbucketname,ParameterValue='$S3_LOGS_BUCKET'"
-ARG_REPOSITORY_BUCKET="ParameterKey=knlrepositorybucketname,ParameterValue='$S3_REPOSITORY_BUCKET'"
 ARG_USERCONTENT_BUCKET="ParameterKey=knlusercontentbucketname,ParameterValue='$S3_USERCONTENT_BUCKET'"
 ARG_API_ENDPOINT="ParameterKey=knlapiendpoint,ParameterValue='$API_ENDPOINT'"
 ARG_CERT_ID="ParameterKey=knlcertificateid,ParameterValue='$CERT_ID'"
 
 
-CFN_CREATE_ARGS="--parameters $ARG_S3_DOMAIN_NAME $ARG_S3_GWT_BUCKET $ARG_S3_LOGS_BUCKET $ARG_API_ENDPOINT $ARG_USERCONTENT_BUCKET $ARG_REPOSITORY_BUCKET $ARG_CERT_ID"
+CFN_CREATE_ARGS="--parameters $ARG_S3_DOMAIN_NAME $ARG_S3_GWT_BUCKET $ARG_S3_LOGS_BUCKET $ARG_API_ENDPOINT $ARG_USERCONTENT_BUCKET $ARG_CERT_ID"
 
 #echo $CFN_CREATE_ARGS
 source $DIR/cfn-create-stack.sh
