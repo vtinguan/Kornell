68dee2e82f6632a218c31b403a20d588be87ae02
Julio Faerman <jfaerman@gmail.com>
2016-05-06 00:21:33 -0300

    Debug header forwarding

--- kornell-api/src/main/scala/kornell/server/dev/ProbesResource.scala
+++ kornell-api/src/main/scala/kornell/server/dev/ProbesResource.scala
@@ -3,6 +3,8 @@ package kornell.server.dev
 import scala.concurrent.ExecutionContext.Implicits.global
 import javax.ws.rs._
 import kornell.server.util.Settings
+import javax.ws.rs.core.Context
+import javax.servlet.http.HttpServletRequest
 
 @Path("/probes")
 @Produces(Array("text/plain"))
@@ -12,4 +14,18 @@ class ProbesResource {
   def settings = Settings
                   .values
                   .mkString("\n")
+
+  
+  @Path("headers")
+  @GET
+  def headers(@Context req:HttpServletRequest) = {
+    val buf:StringBuilder = new StringBuilder
+    val headerNames = req.getHeaderNames
+    while (headerNames.hasMoreElements){
+      val headerName = headerNames.nextElement
+      val headerValue = req.getHeader(headerName)
+      buf.append(s"$headerName = $headerValue \n")
+    }
+    buf.toString
+  }
 }
